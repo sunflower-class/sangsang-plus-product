913982fe2d96b1c20a30d5d5a812147ffc4ac70c
Jeonneung OH <aroma0560@gmail.com>
2025-08-15 12:40:02 +0000

    fix: product price range error

--- src/main/java/com/sangsangplus/productservice/dto/request/ProductCreateRequest.java
+++ src/main/java/com/sangsangplus/productservice/dto/request/ProductCreateRequest.java
@@ -16,10 +16,9 @@ public class ProductCreateRequest {
     @Size(max = 100, message = "카테고리는 100자를 초과할 수 없습니다")
     private String category;
     
-    @NotNull(message = "가격은 필수입니다")
-    @DecimalMin(value = "0.0", inclusive = false, message = "가격은 0보다 커야 합니다")
+    @DecimalMin(value = "-1", inclusive = false, message = "가격은 -1보다 커야 합니다")
     @Digits(integer = 8, fraction = 2, message = "가격 형식이 올바른지 않습니다")
-    private BigDecimal price;
+    private BigDecimal price = BigDecimal.ZERO;
     
     private String brand;
     
